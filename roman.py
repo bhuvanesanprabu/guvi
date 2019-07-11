d1392982d9afd3102abaa09a799c2cba3b9c22be
bhuvanesanprabu <50898822+bhuvanesanprabu@users.noreply.github.com>
2019-07-11 18:06:38 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,7 @@
+a=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+w=str(input())
+for i in range(20):
+  if(w==a[i]):
+    print(b[i])
+    break
