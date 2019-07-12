2122aae571cda9d924436644939421c1dd077db5
bhuvanesanprabu <50898822+bhuvanesanprabu@users.noreply.github.com>
2019-07-12 19:38:25 +0530

    Create char vowel.py

---
+++ char vowel.py
@@ -0,0 +1,18 @@
+a=input()
+b='aeiou'
+c=0
+v=0
+for i in a:
+    if(i in b):
+        print("Vowels")
+        c=c+1
+        break
+if(c==0):
+    for i in a:
+        if(i.isalpha()):
+                print("Consonant")
+                v=v+1
+                break
+if(c==0 and v==0):
+    print("invalid")
+                
