98caa6bc9840086b792278a18433b709662f4cf4
bhuvanesanprabu <50898822+bhuvanesanprabu@users.noreply.github.com>
2019-07-22 18:42:39 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,14 @@
+b=input()
+a=list(map(int,input().split()))
+m=a
+l=[]
+for i in m:
+  if i not in l:
+    l.append(i)
+c=0
+for i in range(0,len(l)):
+  for j in range(i+1,len(l)):
+    for k in range(j+1,len(l)):
+      if(l[i]<l[j]<l[k]):
+        c=c+1
+print(c)
