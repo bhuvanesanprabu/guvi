be4df046c7375d14ff7506bef292bb69260fcc1b
bhuvanesanprabu <50898822+bhuvanesanprabu@users.noreply.github.com>
2019-07-22 18:37:09 +0530

    Create cost.py

---
+++ cost.py
@@ -0,0 +1,13 @@
+a,b=map(str,input(' ').split(' '))
+a1=a
+b1=b
+cc1=0
+if len(a1)>len(b1):
+    a1,b1=b1,a1
+d1=0
+while d1<len(a1):
+      cc1+=(ord(b1[d1])-ord(a1[d1]))
+      d1+=1
+for d1 in range(d1,len(b1)):
+      cc1+=ord(b1[d1])-ord('a')+1
+print(cc1)
