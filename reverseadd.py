5bf0ad88ec9453d97862fa341dba5e5e712d4554
bhuvanesanprabu <50898822+bhuvanesanprabu@users.noreply.github.com>
2019-07-22 19:26:08 +0530

    Create reverseadd.py

---
+++ reverseadd.py
@@ -0,0 +1,17 @@
+n=input()
+Numm23=n
+A=list(map(int,Numm23.split()))
+K=A[1]
+H=input()
+flag=0
+Sv=list(map(int,H.split()))
+for X in range(0,len(Sv)-1):
+    for Y in range(X+1,len(Sv)):
+        if Sv[X]+Sv[Y]==K:
+            print("yes")
+            flag=1
+            break
+    if flag==1:
+        break
+if flag!=1:
+    print("no"
